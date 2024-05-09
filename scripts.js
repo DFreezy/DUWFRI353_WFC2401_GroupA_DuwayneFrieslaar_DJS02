215cf283305eaa376478302db70b3b0a6613a068
Duwayne Frieslaar <155962320+DFreezy@users.noreply.github.com>
2024-05-09 17:11:27 +0200

    Added crash
    I added an event when a non numerical character is entered

--- scripts.js
+++ scripts.js
@@ -22,7 +22,7 @@ form.addEventListener("submit", (event) => {
     }
   
     // Check if either dividend or divider is less than 1
-      result.innerText = dividend || divider ? 1 : "Division not performed. Invalid number provided. Try again";
+      result.innerText = dividend || divider ? 0 : "Division not performed. Invalid number provided. Try again";
       console.error("Error: Invalid number provided. Try again");
 
     
@@ -34,10 +34,10 @@ form.addEventListener("submit", (event) => {
     
     // Display the rounded quotient
     result.innerText = dividend && divider ? roundedQuotient : "Division not performed. Both values are required in inputs. Try again";
+    console.error("Not enough values are present")
   } catch (error) {
     // Handle any errors that occur during calculation
-    console.error(error);
-    result.innerText = "Something critical went wrong. Please reload the page.";
+    let body = document.querySelector(".bodi").innerText = "Something critical went wrong. Please reload the page.";
     // Log error with call stack
     console.error(error.stack);
   };  
